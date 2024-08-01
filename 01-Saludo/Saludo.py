6c85f285773e863b79ee4d7e1f801ff78ae79000
Prof-luis1986 <luisalfonsomartinez@cetis50cdmx.com>
2024-07-31 19:05:25 -0600

    Se suben los archivos a Github

---
+++ 01-Saludo/Saludo.py
@@ -0,0 +1,4 @@
+print("Bienvenido a Python por favor sigue las instrucciones")
+nombre = input("Ingresa tu nombre: ")
+print("Hola", nombre, "espero que disfrutes de Python")
+print("El número de caracteres en tu nombre es:", len(nombre))
